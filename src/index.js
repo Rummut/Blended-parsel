68b5d4eae4a21278534f89b04ba503991df0e4f0
NavrA82 <124286244+NavrA82@users.noreply.github.com>
2023-06-03 13:02:57 +0300

    task-8

--- src/index.js
+++ src/index.js
@@ -60,3 +60,71 @@
 //   ref.buttonRef.textContent = 'Розкрити';
 //   ref.inputRef.type = 'password';
 // }
+
+// Отримуємо необхідні елементи з DOM
+// Задача 1 Створіть HTML сторінку з табличкою,
+//  яка містить список продуктів.Кожен рядок у
+//  табличці повинен містити назву продукту та його ціну.
+//  При натисканні на будь - який рядок у табличці відобразіть
+//  повідомлення з назвою продукту та його ціною.
+
+// const productTable = document.querySelector('#productTable');
+
+// Створіть HTML сторінку з формою,
+// яка містить поле введення для введення
+// імені користувача та кнопку.При натисканні
+// на кнопку відобразіть повідомлення з привітанням з іменем користувача.
+
+// const ref = {
+//   inputRef: document.querySelector('#username'),
+//   buttonRef: document.querySelector('#greetingButton'),
+//   messageRef: document.querySelector('#greetingMessage'),
+// };
+
+// const onButtonClick = () => {
+//   const greetingMessage = `Hello, ${ref.inputRef.value} `;
+//   ref.messageRef.textContent = greetingMessage;
+// };
+// ref.buttonRef.addEventListener('click', onButtonClick);
+
+// Написати функцію, яка буде створювати список // подій клавіатури event.key та event.code
+//Додати класи на список eventList, на елементи //eventCode та eventKey
+
+// const eventThumb = document.querySelector('.eventThumb');
+// const arrey = [];
+// const render = () => {
+//   const markup = arrey
+//     .map(element => {
+//       return `<ul class = "eventList">
+//     <li class = "eventKey">${element.key}</li>
+//     <li class = "eventCode">${element.code}</li>
+//     </ul>`;
+//     })
+//     .join('');
+//   eventThumb.insertAdjacentHTML('beforeend', markup);
+// };
+
+// document.addEventListener('keydown', event => {
+//   arrey.push({ key: event.key, code: event.code });
+// console.log('key: ', event.key);
+// console.log('code: ', event.code);
+//   render();
+// });
+//TODO:==============================================
+/*
+Завдання 9
+Видалити зі списку елементи, які позначені.
+*/
+
+//TODO:==============================================
+/*
+Завдання 10
+Наведено список людей. Зроби можливість сортування списку на ім'я та на прізвище.
+*/
+
+//TODO:==============================================
+/*
+
+Завдання 11
+Наведено список людей. Зроби фільтр на ім'я/прізвище.
+*/
