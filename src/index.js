35cbe36cd074ec5cda19029cb9524613e040ddcf
Rummut <kzantip@gmail.com>
2023-06-03 11:14:01 +0300

    edits 1st

---
+++ src/index.js
@@ -0,0 +1,38 @@
+//TODO:==============================================
+/*
+Завдання 1
+Натискання на кнопку "SHOW ME" має виводити значення з поля введення 
+(дивіться на елементи в html-розмітці)
+*/
+// const ref = {
+//     buttonRef: document.querySelector("#alertButton"),
+//     input: document.querySelector("#alertInput")
+// }
+
+// const onButtonClick = () => {
+//     const inputValue = ref.input.value
+//     ref.input.value = ""
+//     console.log(inputValue)
+// }
+// ref.buttonRef.addEventListener("click", onButtonClick)
+
+//TODO:==============================================
+/*
+Завдання 2
+Після натискання кнопки "SWAP ME" здійснюється обмін вмістом між двома інпутами.
+Ви можете натиснути на неї кілька разів або вручну змінити вміст інпутів.
+*/
+
+// const ref = {
+//     buttonRef: document.querySelector("#swapButton"),
+//     inputLeftRef: document.querySelector("#leftSwapInput"),
+// inputRightRef: document.querySelector("#rightSwapInput")
+// }
+// const onButtonClick = () => {
+//    const leftInputValue = ref.inputLeftRef.value
+//     const rightInputValue = ref.inputRightRef.value
+//     ref.inputLeftRef.value = rightInputValue
+//     ref.inputRightRef.value = leftInputValue
+
+// }
+// ref.buttonRef.addEventListener("click", onButtonClick)
